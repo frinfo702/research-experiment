0c1ab450fde2ade467c11bb3b2d8f7b36d23f1fb
frinfo702 <kenichiro3114@gmail.com>
2026-02-01 16:05:38 +0900

    combined commit new feature
    Add test

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
+from audioop import error
 def main():
     print("Hello from research-experiment!")
+    acutual = new_feature()
+    expected = 1
+    
+    test(expected,acutual)
 
+
+def new_feature():
+    return 1
+
+def test(expected, acutual):
+    if expected != acutual:
+        error
 
 if __name__ == "__main__":
     main()
